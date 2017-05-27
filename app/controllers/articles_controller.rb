16e7d213f38e80a53259d2fefee0996208a8f12d
Rails-Blog <hsz201@email.vccs.edu>
2017-05-27 12:36:42 -0500

    Created functionalities for all the RESTful actions and views for them

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,8 +1,14 @@
 class ArticlesController < ApplicationController
 
 # before_action is used to make sure that a specified method is called in the methods that are specified in the only array
-  before_action :set_article_instance_to_id, only: [:show]
+  before_action :set_article_instance_to_id, only: [:edit, :show, :update, :destroy]
 
+  def index
+    @articles = Article.all
+  end
+
+  # Here the form submission is handled and the POST request goes through
+  # new action is the GET request and create action is the POST request
   def create
     # article_params is used to whitelist the attributes so that mass assignment to a form cannot happen
     @article = Article.new(article_params)
@@ -16,13 +22,35 @@ class ArticlesController < ApplicationController
     end
   end
 
+  # Creates a new article instance with GET request
   def new
     # Without creating the Article model and articles table you cannot do Article.new
     # It throws the error: uninitialized constant Article
     @article = Article.new
   end
 
+  def edit
+  end
+
   def show
+  end
+
+  def update
+    # update is used instead of saved since the article has already been created
+    if @article.update(article_params)
+      flash[:notice] = "Article was successfully updated!"
+
+      redirect_to article_path(@article)
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @article.destroy
+    flash[:notice] = "Article was successfully deleted!"
+    
+    redirect_to articles_path
   end
 
   private
