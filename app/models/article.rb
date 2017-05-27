b3dc78394c2e2f977ee6bbbce4a8c5122c063acf
Rails-Blog <hsz201@email.vccs.edu>
2017-05-27 18:32:42 -0500

    Added bootstrap and designs to pages

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,5 @@
 class Article < ActiveRecord::Base
   # Add some validations to the :title and :description attributes
-  validates :title, presence: true, length: { minimum: 3, maximum: 50 }
-  validates :description, presence: true, length: { minimum: 10, maximum: 500 }
+  validates :title, presence: true, length: { minimum: 3, maximum: 500 }
+  validates :description, presence: true, length: { minimum: 10, maximum: 1000000 }
 end
