06a5b10ace2f27a5f977c8a40da754bdb5d3b659
Rails-Blog <hsz201@email.vccs.edu>
2017-05-28 23:08:12 -0500

    Added the User object and created the table

---
+++ app/models/user.rb
@@ -0,0 +1,5 @@
+class User < ActiveRecord::Base
+  # Create a new feature branch called user-validations and do the validations there
+
+  # Then create another feature branch when adding the foreign key associations call that - userarticle-associations
+end
