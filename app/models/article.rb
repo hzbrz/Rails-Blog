a7aae49c2df2d58bf55edb31c9667c9d8a607e3f
Rails-Blog <hsz201@email.vccs.edu>
2017-05-29 00:06:02 -0500

    Added one to many User association to Article.

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,7 @@
 class Article < ActiveRecord::Base
+  belongs_to :user
   # Add some validations to the :title and :description attributes
   validates :title, presence: true, length: { minimum: 3, maximum: 500 }
   validates :description, presence: true, length: { minimum: 10, maximum: 1000000 }
+  validates :user_id, presence: true
 end
