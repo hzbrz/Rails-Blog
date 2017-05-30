da7d9f58a04fb19d37e4edbd183d66dafdba9b75
Rails-Blog <hsz201@email.vccs.edu>
2017-05-29 23:44:11 -0500

    Added users listing and profile viewer

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,11 @@
 class UsersController < ApplicationController
+
+  before_action :set_user_instance_to_id, only: [:edit, :show, :update]
+
+  def index
+    @users = User.all
+  end
+
   def new
     @user = User.new
   end
@@ -13,7 +20,27 @@ class UsersController < ApplicationController
     end
   end
 
+  def edit
+  end
+
+  def show
+  end
+
+  def update
+    if @user.update(user_params)
+      flash[:success] = "Your account was successfully updated!"
+      redirect_to articles_path
+    else
+      render 'edit'
+    end
+  end
+
   private
+
+    def set_user_instance_to_id
+      @user = User.find(params[:id])
+    end
+
     def user_params
       params.require(:user).permit(:username, :email, :password)
     end
