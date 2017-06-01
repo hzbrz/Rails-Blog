e32d5c8d05066d50c06b57502e9d8412d7171239
Rails-Blog <hsz201@email.vccs.edu>
2017-06-01 09:04:52 -0500

    Added some more designs and tested that everything works

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,13 +11,13 @@ class UsersController < ApplicationController
   def new
     @user = User.new
   end
-  
+
   def create
     @user = User.new(user_params)
     if @user.save
       session[:user_id] = @user.id
       flash[:success] = "Welcome to the Rails Blog #{@user.username}"
-      redirect_to user_path(user)
+      redirect_to user_path(@user)
     else
       render 'new'
     end
