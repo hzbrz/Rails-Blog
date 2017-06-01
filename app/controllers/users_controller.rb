e011b28f2c8cb7a3c1fc4e31ee17711e50aaaf0c
Rails-Blog <hsz201@email.vccs.edu>
2017-06-01 02:38:40 -0500

    Fixed some errors with the admin functionalities

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
 
   before_action :set_user_instance_to_id, only: [:edit, :show, :update, :destroy]
-  before_action :require_same_user, only: [:edit, :update]
+  before_action :require_same_user, only: [:edit, :update, :destroy]
   before_action :require_admin, only: [:destroy]
 
   def index
@@ -15,7 +15,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      session[:user_id] = user.id
+      session[:user_id] = @user.id
       flash[:success] = "Welcome to the Rails Blog #{@user.username}"
       redirect_to user_path(user)
     else
@@ -56,7 +56,7 @@ class UsersController < ApplicationController
     end
 
     def require_same_user
-      if logged_in? && current_user != @user
+      if logged_in? && current_user != @user and !current_user.admin?
         flash[:danger] = "You can only edit your own account"
         redirect_to root_path
       end
